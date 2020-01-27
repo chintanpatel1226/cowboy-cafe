0da781b57646c8c5713444686ff72f1290138522
chintanpatel1226 <chintanpatel1226@ksu.edu>
2020-01-27 13:39:44 -0600

    Missing methods.

---
+++ Data/AngryChicken.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class AngryChicken
+    {
+        public double Price
+        {
+            get { return 5.99; }
+        }
+
+        public uint Calories
+        {
+            get { return 190; }
+        }
+
+        private bool bread = true;
+
+        public bool Bread
+        {
+            get { return bread; }
+            set { bread = value; }
+        }
+
+        private bool pickle = true;
+
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bread) instructions.Add("hold bread");
+                if (!pickle) instructions.Add("hold pickle");
+
+                return instructions;
+            }
+        }
+    }
+}
