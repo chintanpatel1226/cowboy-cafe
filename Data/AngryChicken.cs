5f258717a303c0915ac3144364139a83d078746a
chintanpatel1226 <chintanpatel1226@ksu.edu>
2020-01-29 14:06:37 -0600

    Commented AngryChicken.

--- Data/AngryChicken.cs
+++ Data/AngryChicken.cs
@@ -4,20 +4,31 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
+    /// <summary>
+    /// A class representing the Angry Chicken entree.
+    /// </summary>
     public class AngryChicken
     {
+        /// <summary>
+        /// The price of the Angry Chicken
+        /// </summary>
         public double Price
         {
             get { return 5.99; }
         }
 
+        /// <summary>
+        /// The calories of the Angry Chicken
+        /// </summary>
         public uint Calories
         {
             get { return 190; }
         }
 
         private bool bread = true;
-
+        /// <summary>
+        /// If the sandwich has bread
+        /// </summary>
         public bool Bread
         {
             get { return bread; }
@@ -25,13 +36,18 @@ namespace CowboyCafe.Data
         }
 
         private bool pickle = true;
-
+        /// <summary>
+        /// If the sandwich has a pickle
+        /// </summary>
         public bool Pickle
         {
             get { return pickle; }
             set { pickle = value; }
         }
 
+        /// <summary>
+        /// Special instructions for the preparation of the sandwich.
+        /// </summary>
         public List<string> SpecialInstructions
         {
             get
