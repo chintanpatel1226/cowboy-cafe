fe82c19360ad55b242ba2a83a2fc7608a7920d44
chintanpatel1226 <chintanpatel1226@ksu.edu>
2020-02-26 15:07:31 -0600

    Added OrderTests and created OrderSummaryControl

---
+++ Data/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace CowboyCafe.Data
+{
+    public class Order : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The number of the last order.
+        /// </summary>
+        private uint lastOrderNumber;
+
+        /// <summary>
+        /// List of order items.
+        /// </summary>
+        private List<IOrderItem> items = new List<IOrderItem>();
+
+        public IEnumerable<IOrderItem> Items => items.ToArray();
+
+        private double subtotal;
+        public double Subtotal => subtotal;
+
+        public uint OrderNumber { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    
+        public void Add(IOrderItem item)
+        {
+            subtotal += item.Price;
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+
+        public void Remove(IOrderItem item)
+        {
+            subtotal -= item.Price;
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+    }
+}
